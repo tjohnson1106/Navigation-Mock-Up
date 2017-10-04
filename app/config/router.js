1080213f2df935b677ef53cc44d0be2cc42f8bba
Thomas Johnson <tjohnson1106@gmail.com>
2017-10-04 15:20:10 -0400

    added naviagtion stack

--- app/config/router.js
+++ app/config/router.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { TabNavigator } from 'react-navigation';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 
 import Feed from '../screens/Feed';
 import Me from '../screens/Me';
+import UserDetail from '../screens/UserDetail';
+
+export const FeedStack = StackNavigator({
+    Feed: {
+        screen: Feed,
+        navigationOptions:{
+            title: 'Feed',
+        },
+    },
+    Details: {
+        screen: UserDetail,
+        navigationOptions: ({ navigation }) => ({
+            title: `${navigation.state.params.name.first.toUpperCase()} ${navigation.state.params.name.last.toUpperCase()}` ,
+        }),
+    },
+});
 
 export const Tabs = TabNavigator({
     Feed: {
-        screen: Feed,
+        screen: FeedStack, //Replaced Feed with Feedstack
         navigationOptions: {
             tabBarLabel: 'Feed',
             tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />
